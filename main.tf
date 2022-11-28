8b215f005c7a6f993c43fc0dc950e21e2ea023fc
Seun Jonathanm <seund@yahoo.com>
2022-11-27 20:42:27 -0700

    env_new

--- main.tf
+++ main.tf
@@ -10,31 +10,11 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
-  client_id       = "${var.ARM_CLIENT_ID}"
-  client_secret   = "${var.ARM_CLIENT_SECRET}"
-  tenant_id       = "${var.ARM_TENANT_ID}"
+  # subscription_id = var.ARM_SUBSCRIPTION_ID
+  # client_id       = var.ARM_CLIENT_ID
+  # client_secret   = var.ARM_CLIENT_SECRET
+  # tenant_id       = var.ARM_TENANT_ID
  }
-
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "ARM SUBSCRIPTION"
-  
-}
-
-variable "ARM_CLIENT_ID" {
-  description = "ARMCLIENTID"
-  default = ""
-}
-
-variable "ARM_CLIENT_SECRET" {
-  description = "ARMCLIENTSECRET"
-  default = ""
-}
-
-variable "ARM_TENANT_ID" {
-  description = "ARMTENANTID"
-  default = ""
-}
 
 resource "azurerm_resource_group" "example" {
   name     = "myterrasucceed"
