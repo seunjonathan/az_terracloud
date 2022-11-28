5efd5e9ec8590157fb86ea79778e13ac90a9b909
Seun Jonathanm <seund@yahoo.com>
2022-11-28 14:59:05 -0700

    examcorrected

--- main.tf
+++ main.tf
@@ -30,6 +30,16 @@ resource "azurerm_storage_account" "example" {
   is_hns_enabled           = "true"
 }
 
+resource "azurerm_storage_account" "example1" {
+  name                     = "examplestorageaccseun2"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
 
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "gen1" {
@@ -38,8 +48,8 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "gen1" {
   
   }
 
-  resource "azurerm_storage_data_lake_gen2_filesystem" "gen2" {
-  name               = "gen2"
-  storage_account_id = azurerm_storage_account.example.id
+  resource "azurerm_storage_data_lake_gen2_filesystem" "gennew" {
+  name               = "gennew"
+  storage_account_id = azurerm_storage_account.example1.id
   
   }
