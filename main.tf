9037c41427fcd302c31417f943c7597440e2c5bb
Seun Jonathanm <seund@yahoo.com>
2022-11-27 19:13:11 -0700

    env

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
+  features {}
+  
   subscription_id = var.ARM_SUBSCRIPTION_ID
   client_id       = var.ARM_CLIENT_ID
   client_secret   = var.ARM_CLIENT_SECRET
   tenant_id       = var.ARM_TENANT_ID
-  features {}
  }
 
 resource "azurerm_resource_group" "example" {
