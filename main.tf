fae7222e02fecee74881f217d26da9fbf2763db2
Seun Jonathanm <seund@yahoo.com>
2022-11-27 19:37:20 -0700

    env

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
+  subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
+  client_id       = "${var.ARM_CLIENT_ID}"
+  client_secret   = "${var.ARM_CLIENT_SECRET}"
+  tenant_id       = "${var.ARM_TENANT_ID}"
  }
 
 variable "ARM_SUBSCRIPTION_ID" {
@@ -23,17 +23,17 @@ variable "ARM_SUBSCRIPTION_ID" {
 
 variable "ARM_CLIENT_ID" {
   description = "ARMCLIENTID"
-  default = ""
+  # default = ""
 }
 
 variable "ARM_CLIENT_SECRET" {
   description = "ARMCLIENTSECRET"
-  default = ""
+  # default = ""
 }
 
 variable "ARM_TENANT_ID" {
   description = "ARMTENANTID"
-  default = ""
+  # default = ""
 }
 
 resource "azurerm_resource_group" "example" {
