834c0f9d315b7a62a5693dfcbbbdcefe50cc78f4
Seun Jonathanm <seund@yahoo.com>
2022-11-28 23:18:39 -0700

    new synapse

--- main.tf
+++ main.tf
@@ -30,17 +30,6 @@ resource "azurerm_storage_account" "example" {
   is_hns_enabled           = "true"
 }
 
-resource "azurerm_storage_account" "example1" {
-  name                     = "examplestorageaccseun2"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  is_hns_enabled           = "true"
-}
-
-
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "gen1" {
   name               = "gen1"
@@ -48,12 +37,7 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "gen1" {
   
   }
 
-  resource "azurerm_storage_data_lake_gen2_filesystem" "gennew" {
-  name               = "gennew"
-  storage_account_id = azurerm_storage_account.example1.id
-  
-  }
-
+ 
   resource "azurerm_data_factory" "seunadf" {
   name                = "seunadf"
   location            = azurerm_resource_group.example.location
@@ -63,4 +47,26 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "gen1" {
 resource "azurerm_data_factory_pipeline" "exampleadfp" {
   name            = "exampleadfp"
   data_factory_id = azurerm_data_factory.seunadf.id
+}
+
+#Creating Synapse things dependent on ADLS above
+
+resource "azurerm_synapse_workspace" "seunwksp" {
+  name                                 = "seunwksp"
+  resource_group_name                  = azurerm_resource_group.example.name
+  location                             = azurerm_resource_group.example.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.gen1.id
+  sql_administrator_login              = "seunjonathan"
+  sql_administrator_login_password     = "Gre@tness123"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_synapse_sql_pool" "seunsqlpool" {
+  name                 = "seunsqlpool"
+  synapse_workspace_id = azurerm_synapse_workspace.seunwksp.id
+  sku_name             = "DW100c"
+  create_mode          = "Default"
 }
